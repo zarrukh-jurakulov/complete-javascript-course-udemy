ffe11a125edc50ecabef44727d9052a13b6b7eeb
Zarrukh Jurakulov <zarrukhjurakulov474@gmail.com>
2023-08-06 18:59:32 +0500

    basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -45,7 +45,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -59,3 +58,34 @@ job = "teacher";
 
 lastname = "Schmedtmann";
 console.log(lastname);
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assigment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
